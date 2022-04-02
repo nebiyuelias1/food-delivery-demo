e59acfcd71a657bb4050671c98b53370764b15cd
eliasn <nebiyuelias1@gmail.com>
2022-04-02 13:47:29 +0300

    Add restaurants list and about component

--- food-delivery-web/src/app/app-routing.module.ts
+++ food-delivery-web/src/app/app-routing.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
 import { AppComponent } from './app.component';
 import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component';
+import { RestaurantsListComponent } from './restaurants-list/restaurants-list.component';
 
 const routes: Routes = [
   {
     path: 'detail',
     component: RestaurantDetailComponent
+  },
+  {
+    path: 'restaurants',
+    component: RestaurantsListComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
   }
 ];
 
