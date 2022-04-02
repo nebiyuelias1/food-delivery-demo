7e208f4c4991b91b80ef5019f7221b69bf632c92
eliasn <nebiyuelias1@gmail.com>
2022-04-02 15:14:46 +0300

    Work on home page

--- food-delivery-web/src/app/app-routing.module.ts
+++ food-delivery-web/src/app/app-routing.module.ts
@@ -1,31 +1,42 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { MainComponent } from './main/main.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component';
 import { RestaurantsListComponent } from './restaurants-list/restaurants-list.component';
 
 const routes: Routes = [
   {
+    path: '',
+    component: HomeComponent
+  },
+  {
     path: 'detail',
-    component: RestaurantDetailComponent
+    component: RestaurantDetailComponent,
   },
   {
     path: 'restaurants',
-    component: RestaurantsListComponent
+    component: RestaurantsListComponent,
   },
   {
     path: 'about',
-    component: AboutComponent
+    component: AboutComponent,
   },
   {
     path: '**',
-    component: NotFoundComponent
-  }
+    redirectTo: '404',
+  },
+  {
+    path: '404',
+    component: NotFoundComponent,
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
